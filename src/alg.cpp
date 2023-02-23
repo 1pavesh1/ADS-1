89413edcf06085e9922727b222895e19097a7205
1pavesh1 <125039843+1pavesh1@users.noreply.github.com>
2023-02-23 15:43:51 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,62 +2,60 @@
 #include <cstdint>
 #include "alg.h"
 bool checkPrime(uint64_t value) {
-	uint64_t count = 0;
-	for (int i = 2; i*i <= value; ++i) {
-		if (value % i == 0) {
-			return false;
-		}
+uint64_t count = 0;
+for (int i = 2; i*i <= value; ++i) {
+	if (value % i == 0) {
+		return false;
 	}
-	return true;
+}
+return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-	uint64_t count1 = 0, count2 = 0;
-	for (int i = 1; i <= 10000; i++) {
-		for (int j = i; j >= 1; j--) {
-			if (i % j == 0) {
-				count1++;
-			}
-		}
-		if (count1 == 2) {
-			count2++;
-			count1 = 0;
-		}
-		if (count2 == n) {
-			return i;
-		}
-		else {
-			count1 = 0;
+uint64_t count1 = 0, count2 = 0;
+for (int i = 1; i <= 10000; i++) {
+	for (int j = i; j >= 1; j--) {
+		if (i % j == 0) {
+			count1++;
 		}
 	}
-	return 0;
+	if (count1 == 2) {
+		count2++;
+		count1 = 0;
+	}
+	if (count2 == n) {
+		return i;
+	}
+	else
+		count1 = 0;
+}
+return 0;
 }
 
 uint64_t nextPrime(uint64_t value) {
-	uint64_t count = 0;
-	for (int i = value + 1; i <= 10000; i++) {
-		for (int j = i; j >= 1; j--) {
-			if (i % j == 0) {
-				count++;
-			}
-		}
-		if (count == 2) {
-			return i;
-			count = 0;
-		}
-		else {
-			count = 0;
+uint64_t count = 0;
+for (int i = value + 1; i <= 10000; i++) {
+	for (int j = i; j >= 1; j--) {
+		if (i % j == 0) {
+			count++;
 		}
 	}
-	return 0;
+	if (count == 2) {
+		return i;
+		count = 0;
+	}
+	else
+		count = 0;
+}
+return 0;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-	uint64_t count = 0, sum = 0;
-	for (int i = 0; i < hbound; i++) {
-		if (checkPrime(i)) {
-			sum += i;
-		}
+uint64_t count = 0, sum = 0;
+for (int i = 0; i < hbound; i++) {
+	if (checkPrime(i)) {
+		sum += i;
 	}
-	return sum - 1;
+}
+return sum - 1;
 }
