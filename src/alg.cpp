d688441f6d4c6fbfd0c90d9c80756923b128d2be
1pavesh1 <125039843+1pavesh1@users.noreply.github.com>
2023-02-23 17:26:45 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,9 +4,9 @@
 bool checkPrime(uint64_t value) {
   uint64_t count = 0;
   for (int i = 2; i*i <= value; ++i) {
-	  if (value % i == 0) {
-		  return false;
-	  }
+    if (value % i == 0) {
+      return false;
+    }
   }
   return true;
 }
@@ -14,19 +14,19 @@ bool checkPrime(uint64_t value) {
 uint64_t nPrime(uint64_t n) {
   uint64_t count1 = 0, count2 = 0;
   for (int i = 1; i <= 10000; i++) {
-	  for (int j = i; j >= 1; j--) {
-		  if (i % j == 0) {
-			  count1++;
-		  }
-	  }
-	  if (count1 == 2) {
-		  count2++;
-		  count1 = 0;
-	  }
-	  if (count2 == n) {
-		  return i;
-	  }
-		  count1 = 0;
+    for (int j = i; j >= 1; j--) {
+      if (i % j == 0) {
+        count1++;
+      }
+    }
+    if (count1 == 2) {
+      count2++;
+      count1 = 0;
+    }
+    if (count2 == n) {
+      return i;
+    }
+      count1 = 0;
   }
   return 0;
 }
@@ -34,16 +34,16 @@ uint64_t nPrime(uint64_t n) {
 uint64_t nextPrime(uint64_t value) {
   uint64_t count = 0;
   for (int i = value + 1; i <= 10000; i++) {
-	  for (int j = i; j >= 1; j--) {
-		  if (i % j == 0) {
-			  count++;
-		  }
-	  }
-	  if (count == 2) {
-		  return i;
-		  count = 0;
-	  }
-		  count = 0;
+    for (int j = i; j >= 1; j--) {
+      if (i % j == 0) {
+        count++;
+      }
+    }
+    if (count == 2) {
+      return i;
+      count = 0;
+    }
+      count = 0;
   }
   return 0;
 }
@@ -51,9 +51,9 @@ uint64_t nextPrime(uint64_t value) {
 uint64_t sumPrime(uint64_t hbound) {
   uint64_t count = 0, sum = 0;
   for (int i = 0; i < hbound; i++) {
-	  if (checkPrime(i)) {
-		  sum += i;
-	  }
+    if (checkPrime(i)) {
+      sum += i;
+    }
   }
   return sum - 1;
 }
